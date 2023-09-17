0fb1e3ac5d8d43cdce78ed7d3a93ea0dbfb8a81f
Gabriel Ferraz <135299312+gabrielferrazz@users.noreply.github.com>
2023-09-17 19:52:37 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,36 +1,49 @@
-import cors from "cors"
-import express from "express"
+import cors from "cors";
+import express from "express";
 
-import { convert } from "./convert.js"
-import { download } from "./download.js"
-import { transcribe } from "./transcribe.js"
-import { summarize } from "./summarize.js"
+import { convert } from "./convert.js";
+import { download } from "./download.js";
+import { transcribe } from "./transcribe.js";
+import { summarize } from "./summarize.js";
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+
+const whitelist = ['https://aisummarizegf.netlify.app'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 
 app.get("/summary/:id", async (request, response) => {
   try {
-    await download(request.params.id)
-    const audioConverted = await convert()
-    const result = await transcribe(audioConverted)
+    await download(request.params.id);
+    const audioConverted = await convert();
+    const result = await transcribe(audioConverted);
 
-    return response.json({ result })
+    return response.json({ result });
   } catch (error) {
-    console.log(error)
-    return response.json({ error })
+    console.log(error);
+    return response.json({ error });
   }
-})
+});
 
 app.post("/summary", async (request, response) => {
   try {
-    const result = await summarize(request.body.text)
-    return response.json({ result })
+    const result = await summarize(request.body.text);
+    return response.json({ result });
   } catch (error) {
-    console.log(error)
-    return response.json({ error })
+    console.log(error);
+    return response.json({ error });
   }
-})
+});
 
-app.listen(3333, () => console.log("Server is running on port 3333"))
+app.listen(3333, () => console.log("Server is running on port 3333"));
